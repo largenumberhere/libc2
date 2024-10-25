2c795e5c05926c627dfd8edbdaee5e03ddea13b3
rose <largenumbergoeshere@gmail.com>
2024-10-26 06:38:09 +1100

    Updated syscall interface to match arm64

--- lib2/syscall.c
+++ lib2/syscall.c
@@ -6,15 +6,6 @@
 #define NULL (void*)0
 
 static size_t syscall6(int id, size_t reg1, size_t reg2, size_t reg3, size_t reg4, size_t reg5, size_t reg6) {
-	/**
-	 * x86_64 syscall interface
-	 * rax, rdx = (rax, rdi, rsi, rdx, r10, r8, r9)
-	 * 
-	 * c abi
-	 * rax = (rdi, rsi, rdx, rcx, r8, r9)
-	 * 
-	 */
-	
 	register size_t rax asm("rax") = id;
 	register size_t rdi asm("rdi") = reg1;
 	register size_t rsi asm("rsi") = reg2;
@@ -23,7 +14,6 @@ static size_t syscall6(int id, size_t reg1, size_t reg2, size_t reg3, size_t reg
 	register size_t r8  asm("r8")  = reg5;
 	register size_t r9  asm("r9")  = reg6; 
 
-	// make a syscall, outputting to rax, taking in implicit arguments rax, rdi, rsi and rdx
 	asm volatile(
 		"syscall"
 		: "+r" (rax)
@@ -46,11 +36,14 @@ static size_t syscall3(size_t id, size_t reg1, size_t reg2, size_t reg3) {
 	return syscall6(id, reg1, reg2, reg3, NULL, NULL, NULL);
 }
 
+static size_t syscall4(size_t id, size_t reg1, size_t reg2, size_t reg3, size_t reg4) {
+	return syscall6(id, reg1, reg2, reg3, reg4, (size_t)NULL, (size_t)NULL);
+}
 
 
 
 size_t sys_write(int fd, const void* string, size_t len) {
-	size_t v = syscall3((size_t)1, (size_t)fd, (size_t)string, (size_t)len);
+	size_t v = syscall3(1, (size_t)fd, (size_t)string, (size_t)len);
     return v;
 }
 
@@ -61,7 +54,7 @@ void sys_exit(int status) {
 }
 
 size_t sys_read(int fd, char* buffer, size_t count) {
-    return syscall3(0,fd, (size_t) buffer, count);
+    return syscall3(0, (size_t) fd, (size_t) buffer, (size_t) count);
 }
 
 size_t sys_brk(size_t brk) {
@@ -70,9 +63,16 @@ size_t sys_brk(size_t brk) {
 }
 
 
+//https://sites.uclouvain.be/SystInfo/usr/include/linux/fcntl.h.html
+#define AT_FDCWD                -100    /* Special value used to indicate
+                                           openat should use the current
+                                           working directory. */
+
 int sys_open(const char* pathname, int flags, int mode) {
-	return (int) syscall3(2, pathname, flags, mode);
+	return (int) syscall4(257, AT_FDCWD, pathname, flags, mode);
 }
+
+
 
 void *sys_mmap(void *addr, unsigned long long length, int prot, int flags, int fd, long offset) {
 	void* ret = (void*) syscall6(9, (size_t) addr, (size_t)length, (size_t)prot, (size_t)flags, (size_t)fd, (size_t) offset);
@@ -80,19 +80,7 @@ void *sys_mmap(void *addr, unsigned long long length, int prot, int flags, int f
 }
 
 int sys_munmap(void* addr, size_t length) {
-	// int ret = (int) syscall2(11, (unsigned long)addr, (size_t)length);
-	// return ret;
-	volatile register size_t rax asm("rax") = (size_t) 11;
-  	volatile register size_t rdi asm("rdi") = (unsigned long) addr;
-  	volatile register size_t rsi asm("rsi") = (size_t) length;
-
-	asm volatile("syscall"
-	:"+r" (rax)
-	:"r" (rax), "r" (rdi), "r" (rsi)
-	: "memory"
-	);
-
-	return (int) rax;
+	return (int) syscall2(11, (size_t)addr, (size_t)length);
 }
 
 int sys_close(int fd) {
