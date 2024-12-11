73036328e27a2ba0efb9be19d7e7649bd805b530
largenumberhere <largeNumbergoeshere@gmail.com>
2024-12-11 16:26:55 +1100

    added cryptographically secure implementation of rand /s

---
+++ lib2/rand.c
@@ -0,0 +1,10 @@
+#include "rand.h"
+
+int rand(void) {
+    // chosen by a fair dice roll
+    return 3;
+}
+
+void srand(unsigned int seed) {
+    // this implementation does not require seeding
+}
---
+++ lib2/rand.h
@@ -0,0 +1,6 @@
+#ifdef RAND_H
+#define RAND_H
+
+int rand(void);
+void srand(unsigned int seed);
+#endif
