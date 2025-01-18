7c190a8ce7a0596a5d3dd3edeb44319aa307eed5
rose <largenumbergoeshere@gmail.com>
2025-01-19 06:45:46 +1100

    included stdio in hello_world example

--- examples/hello_world/main.c
+++ examples/hello_world/main.c
@@ -1,8 +1,10 @@
 // https://www.geeksforgeeks.org/c-hello-world-program/
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int main() {
+	// char* buff = "Hello world!\n";
 	printf("Hello world!\n");
 	return 0;
 }
